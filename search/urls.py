33f7625341d8d6fc426ae29c708a793929042107
caturwmn <caturwira41@gmail.com>
2025-05-21 00:35:29 +0700

    [FEAT] Implement retrieval augmented generation

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import es_health_check, search, llm_health_check
+from .views import es_health_check, search, llm_health_check, perform_rag
 
 urlpatterns = [
     path('es-check/', es_health_check),
-    path('search/', search),
+    path('', search),
+    path('generate/', perform_rag),
     path('llm-check/', llm_health_check)
 ]
 
--- search/views.py
+++ search/views.py
@@ -17,7 +17,7 @@ def es_health_check(request):
 def search(request):
     if request.method == 'GET':
         query = request.GET.get('q', 'default')
-        res = es.search(index="documents", body={
+        res = es.search(index="medicine", size=10, body={
             "query": {
                 "multi_match": {
                     "query": query,
@@ -26,8 +26,50 @@ def search(request):
             }
         })
         hits = res["hits"]["hits"]
-        results = [{"id": hit["_id"], "score": hit["_score"], "title": hit["_source"]["title"]} for hit in hits]
+        results = [{
+            "id": hit["_id"], 
+            "url": hit["_source"]["url"], 
+            "score": hit["_score"], 
+            "title": hit["_source"]["title"],
+            "text": hit["_source"]["main_text"]
+        } for hit in hits]
         return JsonResponse(results, safe=False)
+    
+def perform_rag(request):
+    try:
+        if request.method == 'GET':
+            query = request.GET.get('q', 'default')
+            res = es.search(index="medicine", size=4, body={
+                "query": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": ["title", "abstract"]
+                    }
+                }
+            })
+            hits = res["hits"]["hits"]
+            results = [hit["_source"]["main_text"] for hit in hits]
+
+            texts = "\n\n".join(results)
+
+            prompt = f"""
+            Answer the question based on the context below:
+
+            Context:
+            {texts}
+
+            Question: {query}    
+            """
+            rag = client.chat.completions.create(
+                model="mistralai/Mistral-7B-Instruct-v0.3",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ],
+            )
+            return JsonResponse(rag, safe=False)
+    except Exception as e:
+        return JsonResponse({"status": "error", "detail": str(e)}, status=500)
+
 def llm_health_check(request):
     try:
         completion = client.chat.completions.create(
